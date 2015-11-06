b1dc61ad8338dec1ade4c3548a9ab0b4eff6abb3
sufyanAbbasi <home@Amylab-1.local>
2015-11-06 17:00:28 -0500

    Tabs Switch and a space for searching

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 function init(){
-
-	function makePost(title, url){
+	var PANO_URL = "http://digitallibrary.vassar.edu/panorama_krpano_embed/"
+	var panoIDList = []; 
+	var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere malesuada leo eget pellentesque. Praesent hendrerit tincidunt velit imperdiet commodo. In porttitor luctus justo sit amet ultricies. Sed vulputate velit ac feugiat iaculis. Nullam lacinia, elit quis pretium molestie, est purus accumsan orci, tristique lacinia neque ipsum nec libero. Maecenas pellentesque, magna id faucibus consequat, nisi libero pharetra nulla, in vehicula sapien nisi imperdiet justo. Proin vestibulum purus quam, ut dictum elit interdum id. Curabitur ut pretium tortor. Duis vehicula aliquam quam egestas semper. Quisque bibendum nulla in commodo efficitur. Vivamus vehicula ligula id consectetur feugiat."
+	function makePost(title, panoID){
 		var postHTML = []; 
 		postHTML.push('<div class="post">');
 		postHTML.push('<a name="'+ title.split(' ').join('_') + '"></a>');
 		postHTML.push('<div class="title">');
 		postHTML.push('<p>' + title + '</p>')
-		postHTML.push('<div class="button x-button"><span style="color:rgb(200,200,200);">&times</span></div>'); 
-		postHTML.push('<div class="button min-button"><span style="color:rgb(200,200,200);">&#8722</span></div>'); 
+		postHTML.push('<div class="button x-button nohighlight"><span style="color:rgb(200,200,200);">&times</span></div>'); 
+		postHTML.push('<div class="button min-button nohighlight"><span style="color:rgb(200,200,200);">&#8722</span></div>'); 
 		postHTML.push('</div>')
 		postHTML.push('<div class="content-area">');
 		postHTML.push('<div class="tab-bar">');
-		postHTML.push('<div class="tab"><p>Panorama</p></div>');
-		postHTML.push('<div class="tab"><p>Information</p></div>');
+		postHTML.push('<div class="tab pano selected nohighlight" data-tab="panorama"><p>Panorama</p></div>');
+		postHTML.push('<div class="tab info nohighlight" data-tab="information"><p>Information</p></div>');
 		postHTML.push('</div>');
-		postHTML.push('<iframe src="' + url + '" allowFullScreen></iframe>');
+		postHTML.push('<iframe class="panorama" src="' + PANO_URL + panoID + '" allowFullScreen></iframe>');
+		postHTML.push('<div class="information" style="display: none;">');
+		postHTML.push('<div class="google-maps"></div>'); 
+		postHTML.push('<h2>About the ' + title + '</h2>');
+		postHTML.push('<p>' + lorem + '</p>');
+		postHTML.push('</div>'); 
 		postHTML.push('</div>'); 
 		postHTML.push('</div>');
 		var postRef = $(postHTML.join(""));
 
 		$("#posts").append(postRef);
-		
 
 		var linkHTML = [];
 		linkHTML.push('<li>');
@@ -33,10 +39,8 @@ function init(){
 
 
 		$(postRef).find(".x-button").click(function(){
-			if(window.confirm("Are you sure you want to delete this panorama?")){
-				$(postRef).remove();
-				$(linkRef).remove();
-			}
+			$(postRef).remove();
+			$(linkRef).remove();
 		})
 
 		var minButton = $(postRef).find(".min-button");
@@ -54,32 +58,70 @@ function init(){
 			$(postContent).toggle();
 
 
-		})
+		});
+
+		var tabs = $(postRef).find(".tab");
+
+		$(tabs).click(function(event){
+			tabSelect(event.currentTarget);
+		});
 
 	}
-
-
 
 	function processPosts(url){
 		$.getJSON(url, function(data){
 			for (var i = 0; i < data.panoramas.length; i++) {
 				var pano = data.panoramas[i]; 
-				var postHTML = []; 
-				makePost(pano.title, pano.url);
+				makePost(pano.title, pano.PID);
 			};
 		});
 	}
 
-	//JSON Link A: https://api.myjson.com/bins/4iayi
-	//JSON Link B: https://api.myjson.com/bins/neyi
+	function processSolrJSON(search){
+		$.ajax({
+			'url': 'http://dg02.vassar.edu:8080/solr/select',
+			'data': {
+				      'indent': 'on',
+				      'start':'0',
+				      'q': search,
+				      'wt':'json',
+				      'rows':'600',
+				      'version':'2.2'},
+			'success': function(data) {
+				var panoList = data.response.docs;
+				for (var i in panoList){
+					var pano = panoList[i];
+					console.log(pano);
+					var panoTitle = pano["dc.title_s"][0]; 
+					var panoID = pano.PID; 
+						
+					panoIDList.push(pano.PID);
+					makePost(panoTitle, panoID); 
+				}
+			},
+			'dataType': 'jsonp',
+  			'jsonp': 'json.wrf'
+		});
+	}
 
-	processPosts("https://api.myjson.com/bins/4iayi");
+	function tabSelect(element){
+		if(!$(element).hasClass('selected')){
+			$(element).siblings().filter('.selected').removeClass('selected');
+			$(element).addClass('selected');
+			switchTabs(element); 
+		}
+	}
 
+	function switchTabs(element){
+		var $pano = $(element).parent().siblings().filter('.panorama'); 
+		var $info = $pano.siblings().filter('.information'); 
+		$($info).toggle();
+		$($pano).toggle();
+	}
 	
+	processPosts("https://api.myjson.com/bins/4xbsi");
 
-
-
-
+	// processSolrJSON('dc.identifier:gothic*');
 
 
 }
