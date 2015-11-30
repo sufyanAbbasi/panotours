4f2e339a30150788a67d93f592a426d614b34e89
sufyanAbbasi <home@Amylab-1.local>
2015-11-30 16:05:33 -0500

    Primitive Cathedral Post

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 var PANO_URL = "http://digitallibrary.vassar.edu/panorama_krpano_embed/";
 var SOLR_URL = "http://dg02.vassar.edu:8080/solr/select";
+
+var THUMBNAIL_BEGIN = "http://digitallibrary.vassar.edu/islandora/object/";
+//PID goes in between, like gothic:1 
+var THUMBNAIL_END = "/datastream/TN/view";
+
 var GOTHIC_NAMESPACE = "gothic:";
 var ALL_GOTHIC = "dc.identifier:gothic*";
-var PANO_TITLE = "dc.title_s:";
-var ACTIVE_PANO = "mods.title_sort:*"; 
-var ALL_PANOS = "+" + ALL_GOTHIC + " +" + ACTIVE_PANO;
+var SEARCH_TITLE = "dc.title_s:";
+var ACTIVE_COLLECTION = "rels.hasModel:*collectionCModel"; 
+var ACTIVE_CATHEDRALS = ACTIVE_COLLECTION; 
+var ACTIVE_PANO = "rels.hasModel:*panoramaCModel"; 
+var ALL_PANOS = ALL_GOTHIC + " AND " + ACTIVE_PANO;
+var ALL_CATHEDRALS = ALL_GOTHIC + " AND " + ACTIVE_CATHEDRALS;
 var PANO_ID = "PID"; 
-var PARENT_DIR = "PARENT_collection_s";
+var PARENT_DIR = "PARENT_collection_s:";
+
 var panoIDList = []; 
+var cathedralNameList = [];
 var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere malesuada leo eget pellentesque. Praesent hendrerit tincidunt velit imperdiet commodo. "
 			+ "In porttitor luctus justo sit amet ultricies. Sed vulputate velit ac feugiat iaculis. Nullam lacinia, elit quis pretium molestie, est purus accumsan orci,"
 			+ "tristique lacinia neque ipsum nec libero. Maecenas pellentesque, magna id faucibus consequat, nisi libero pharetra nulla, in vehicula sapien nisi imperdiet justo."
@@ -17,12 +27,13 @@ var map;
 
 var posts = []; 
 
-var totalNumPanos; 
+var totalNumPanos, totalNumCathedrals; 
 
 function init(){
 	resizeToWindow();
 	// processPosts("https://api.myjson.com/bins/4xbsi");
 	processSolrJSON(ALL_PANOS, processAllPanos);
+	processSolrJSON(ALL_CATHEDRALS, processAllCathedrals);
 
 	var timer = null; 
    	$('#search-bar').keyup(function(){
@@ -88,10 +99,10 @@ function resizeToWindow(){
 	var winWidth = $(window).width()
 
 	if($('#left-sidebar').width() == parseInt($('#left-sidebar').css('min-width'), 10)){
-		$('#search').css('margin-left', (parseInt($('#left-sidebar').css('min-width'), 10) + 10)); 
+		$('#search').css('margin-left', (parseInt($('#left-sidebar').css('min-width'), 10) + 50)); 
 		$('.main-content').css('margin-left', 365); 
 	}else{
-		$('#search').css('margin-left', 'calc(25% + 10px)'); 
+		$('#search').css('margin-left', 'calc(25% + 50px)'); 
 		$('.main-content').css('margin-left', 'calc(65%/2)');
 	}
 }
@@ -100,24 +111,24 @@ function searchAttributes(){
 	 var str = $('#search-bar').val();   
 	 if(str.length == 0){
 	 	$('#results-list ul label').show(); 
-	 	console.log("Show all panos.");
 	 	$('#pano-showing-total').replaceWith('<span id="pano-showing-total">Showing: <span class="avoidWrapping">' + totalNumPanos + ' of ' + totalNumPanos + '</span></span>');
+	 	$('#cathedral-showing-total').replaceWith('<span id="cathedral-showing-total">Showing: <span class="avoidWrapping">' + totalNumCathedrals + ' of ' + totalNumCathedrals + '</span></span>');
 	 	return;
 	 }
 	 var wordList = str.split(" ") || [];
-     var searchConditions = ALL_PANOS; 
-
      var allSpaces = true;
 
+     var wordSearchConditions = ""; 
      for(var i = 0, len = wordList.length; i < len; i++){
      	if(wordList[i].length != 0){
-     		searchConditions += " +" + PANO_TITLE + "*" + wordList[i] + "*"; 
+     		wordSearchConditions += " AND " + SEARCH_TITLE + "*" + wordList[i] + "*"; 
      		allSpaces = false;
      	}
      }
      
      if(!allSpaces){
-     	processSolrJSON(searchConditions, processSearch);
+     	processSolrJSON(ALL_PANOS + wordSearchConditions, processPanoSearch);
+     	processSolrJSON(ALL_CATHEDRALS + wordSearchConditions, processCathedralSearch);
      }
 }
 
@@ -126,13 +137,12 @@ function searchAttributes(){
 // 	$.getJSON(url, function(data){
 // 		for (var i = 0; i < data.panoramas.length; i++) {
 // 			var pano = data.panoramas[i]; 
-// 			makePost(pano.title, pano.PID.split("gothic:"));
+// 			makePanoPost(pano.title, pano.PID.split("gothic:"));
 // 		};
 // 	});
 // }
 
 function processSolrJSON(search, handler){
-	console.log("processing search: " + search)
 	$.ajax({
 		'url': SOLR_URL,
 		'data': {
@@ -144,8 +154,8 @@ function processSolrJSON(search, handler){
 			      'version':'2.2'},
 		'success': function(data) {
 			var numFound = data.response.numFound;
-			var panoList = data.response.docs;
-			handler(panoList, numFound);
+			var foundList = data.response.docs;
+			handler(foundList, numFound);
 		},
 		'dataType': 'jsonp',
 			'jsonp': 'json.wrf'
@@ -156,52 +166,163 @@ function processAllPanos(panoList, numFound){
 	for (var i in panoList){
 		var pano = panoList[i];
 		var panoTitle = pano["dc.title_s"][0]; 
-		var panoID = parseInt(pano.PID.split("gothic:")[1]); 
-		var nonPanos = 0; 
-		if(!isNaN(panoID)){
-			panoIDList.push(pano.PID);
-			$('#pano-title').after('<label class="pano-list-elements"><li class="nohighlight"><input class="pano-checkbox" type="checkbox" data-title="' + panoTitle +
-				'" data-id="'+ panoID + '" onclick=clickPano(this)><div class="custom-checkbox"></div><p>' + panoTitle.split('_').join(' ') + '</p></li></label>');
-			// makePost(panoTitle, panoID); 
-		}else{
-			console.log("Found: " + panoTitle);
-			nonPanos++; 
+		var panoID = pano.PID.split("gothic:")[1]; 
+		panoIDList.push(pano.PID);
+		$('#pano-title').after('<label class="pano-label result-list-elements nohighlight"><li><input class="pano-checkbox" type="checkbox" data-title="' + panoTitle +
+			'" data-id="'+ panoID + '" onclick=clickPano(this)><div class="custom-checkbox"></div><p>' + panoTitle.split('_').join(' ') + '</p></li></label>');
 		}
-		
-	}
 
-	totalNumPanos = numFound - nonPanos; 
+	totalNumPanos = numFound;
 
 	$('#pano-showing-total').replaceWith('<span id="pano-showing-total">Showing: <span class="avoidWrapping">' + totalNumPanos + ' of ' + totalNumPanos + '</span></span>');
 }
 
-function processSearch(panoList, numFound){
-	$('.pano-list-elements').hide(); 
+function processAllCathedrals(cathedralList, numFound){
+	for (var i in cathedralList){
+		var cathedral = cathedralList[i];
+		var cathedralTitle = cathedral["dc.title_s"][0]; 
+		var cathedralID = cathedral.PID.split("gothic:")[1]; 
+		$('#cathedral-title').after('<label class="cathedral-label result-list-elements nohighlight"><li><input class="cathedral-checkbox" type="checkbox" data-title="' + cathedralTitle +
+			'" data-id="'+ cathedralID + '" onclick=clickCathedral(this)><div class="custom-checkbox"></div><p>' + cathedralTitle.split('_').join(' ') + '</p></li></label>');
+	}
+
+	totalNumCathedrals = numFound; 
+
+	$('#cathedral-showing-total').replaceWith('<span id="cathedral-showing-total">Showing: <span class="avoidWrapping">' + totalNumCathedrals + ' of ' + totalNumCathedrals + '</span></span>');
+}
+
+function processPanoSearch(panoList, numFound){
+	$('.pano-label').hide(); 
 
 	for (var i in panoList){
-		var nonPanos = 0; 
 		var pano = panoList[i];
-		var panoID = parseInt(pano.PID.split("gothic:")[1]);
-		if(!isNaN(panoID)){
-			$('input[data-id="'+ panoID + '"]').parent().parent().show();
-		}else{
-			nonPanos++;
-		}
-		
+		var panoID = pano.PID.split("gothic:")[1];
+		$('input[data-id="'+ panoID + '"]').parent().parent().show();		
 	}
-	$('#pano-showing-total').replaceWith('<span id="pano-showing-total">Showing: <span class="avoidWrapping">' + (numFound-nonPanos) + ' of ' + totalNumPanos + '</span></span>');
+	$('#pano-showing-total').replaceWith('<span id="pano-showing-total">Showing: <span class="avoidWrapping">' + numFound + ' of ' + totalNumPanos + '</span></span>');
+}
+
+function processCathedralSearch(cathedralList, numFound){
+	$('.cathedral-label').hide(); 
+
+	for (var i in cathedralList){
+		var cathedral = cathedralList[i];
+		var cathedralID = cathedral.PID.split("gothic:")[1];
+		$('input[data-id="'+ cathedralID + '"]').parent().parent().show();		
+	}
+	$('#cathedral-showing-total').replaceWith('<span id="cathedral-showing-total">Showing: <span class="avoidWrapping">' + numFound + ' of ' + totalNumCathedrals + '</span></span>');
 }
 
 
 function clickPano(input){
 	if(input.checked) {
-        makePost(input.dataset.title, input.dataset.id); 
+        makePanoPost(input.dataset.title, input.dataset.id); 
       }else{
         removePost(input.dataset.id); 
       }
 }
 
-function makePost(title, panoID){
+function clickCathedral(input){
+	if(input.checked){
+		makeCathedralPost(
+			input.dataset.title, 
+			input.dataset.id)
+	}else{
+		removePost(input.dataset.id);
+	}
+}
+
+function makeCathedralPost(title, cathedralID){
+	var postHTML = []; 
+	postHTML.push('<div class="post">');
+	postHTML.push('<a name="'+ title.split(' ').join('_') + '"></a>');
+	postHTML.push('<div class="title">');
+	postHTML.push('<div class="cathedral-icon"></div>');
+	postHTML.push('<p>' + title + '</p>')
+	postHTML.push('<div class="button x-button nohighlight">&times</div>'); 
+	postHTML.push('<div class="button min-button nohighlight">&#8722</div>'); 
+	postHTML.push('</div>')
+	postHTML.push('<div class="content-area">');
+	postHTML.push('<div class="tab-bar">');
+	postHTML.push('<div class="tab panos selected nohighlight" data-tab="panoramas"><p>Panoramas</p></div>');
+	postHTML.push('<div class="tab info middle-tab nohighlight" data-tab="information"><p>Information</p></div>');
+	postHTML.push('<div class="tab timeline  middle-tab nohighlight" data-tab="timeline"><p>Timeline</p></div>');
+	postHTML.push('<div class="tab resources nohighlight" data-tab="resources"><p>Resources</p></div>');
+	postHTML.push('</div>');
+	postHTML.push('<div class="panoramas tab-content">');
+	postHTML.push('<div class="pano-list"><div>');
+	postHTML.push('<div class="pano"><div>');  
+	postHTML.push('</div>');
+	postHTML.push('<div class="information tab-content" style="display: none;">');
+	postHTML.push('<div class="google-maps"></div>'); 
+	postHTML.push('<h2>About the ' + title + '</h2>');
+	postHTML.push('<p>' + lorem + '</p>');
+	postHTML.push('</div>'); 
+	postHTML.push('<div class="timeline tab-content" style="display: none;">');
+	postHTML.push('<h2>Timeline of ' + title + '</h2>');
+	postHTML.push('</div>');
+	postHTML.push('<div class="resources tab-content" style="display: none;">');
+	postHTML.push('<h2>Resources for ' + title + '</h2>');
+	postHTML.push('</div>');  
+	postHTML.push('</div>'); 
+	postHTML.push('</div>');
+	var postRef = $(postHTML.join(""));
+
+	$("#posts").append(postRef);
+
+	var linkHTML = [];
+	linkHTML.push('<li>');
+	linkHTML.push('<a href="#' + title.split(' ').join('_') + '">'); 
+	linkHTML.push('<h2>' + title + '</h2>'); 
+	linkHTML.push('</a></li>');
+	var linkRef = $(linkHTML.join(""));
+	
+	$("#contents > ul").append(linkRef);
+
+	posts.push({
+		title : title,
+		id : cathedralID,
+		postRef : postRef,
+		linkRef : linkRef,
+	}); 
+
+	processSolrJSON(ALL_PANOS + ' AND ' + PARENT_DIR + '"' + title + '"' , function(panoList, numFound){
+		$panoDiv = $(postRef).find(".panoramas"); 
+		// for (var i in panoList){
+		// 	var pano = panoList[i];
+		// 	var panoID = pano.PID.split("gothic:")[1];
+		// 	$($panoDiv).append('<img class="tile" data-pid="' + panoList[i]
+		// 		+'" src="' + THUMBNAIL_BEGIN + GOTHIC_NAMESPACE + panoID + THUMBNAIL_END + '">');
+		// }
+	})
+
+	$(postRef).find(".x-button").click(function(){
+		removePost(cathedralID);
+	})
+
+	var minButton = $(postRef).find(".min-button");
+
+	$(minButton).click(function(){
+		var postContent = $(postRef).find(".content-area"); 
+		if ($(postContent).is(":visible")){
+			$(postRef).height("50px");
+			$(minButton).children().html("&#43");
+		}else{
+			$(postRef).height("75%");
+			$(minButton).children().html("&#8722");
+		}
+
+		$(postContent).toggle();
+	});
+
+	var tabs = $(postRef).find(".tab");
+
+	$(tabs).click(function(event){
+		tabSelect(event.currentTarget);
+	});
+}
+
+function makePanoPost(title, panoID){
 	var postHTML = []; 
 	postHTML.push('<div class="post">');
 	postHTML.push('<a name="'+ title.split(' ').join('_') + '"></a>');
@@ -216,9 +337,9 @@ function makePost(title, panoID){
 	postHTML.push('<div class="tab pano selected nohighlight" data-tab="panorama"><p>Panorama</p></div>');
 	postHTML.push('<div class="tab info nohighlight" data-tab="information"><p>Information</p></div>');
 	postHTML.push('</div>');
-	postHTML.push('<iframe class="panorama" src="' + PANO_URL + GOTHIC_NAMESPACE + panoID + '" allowFullScreen></iframe>');
-	postHTML.push('<div class="information" style="display: none;">');
-	postHTML.push('<div class="google-maps"></div>'); 
+	postHTML.push('<iframe class="panorama tab-content" src="' + PANO_URL + GOTHIC_NAMESPACE + panoID + '" allowFullScreen></iframe>');
+	postHTML.push('<div class="information tab-content" style="display: none;">');
+	postHTML.push('<div class="google-maps tab-content"></div>'); 
 	postHTML.push('<h2>About the ' + title + '</h2>');
 	postHTML.push('<p>' + lorem + '</p>');
 	postHTML.push('</div>'); 
@@ -263,8 +384,6 @@ function makePost(title, panoID){
 		}
 
 		$(postContent).toggle();
-
-
 	});
 
 	var tabs = $(postRef).find(".tab");
@@ -298,10 +417,10 @@ function tabSelect(element){
 }
 
 function switchTabs(element){
-	var $pano = $(element).parent().siblings('.panorama'); 
-	var $info = $pano.siblings('.information'); 
-	$($info).toggle();
-	$($pano).toggle();
+	var $currentTab = $(element).parent().siblings('.' + element.dataset.tab); 
+	var $restTab = $currentTab.siblings('.tab-content').not($currentTab); 
+	$($currentTab).show();
+	$($restTab).hide();
 }
 
 function initializeGoogleMaps(){
